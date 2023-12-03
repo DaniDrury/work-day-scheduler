2c79befcea3c1b11a201412ef000b1e1d754d583
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2023-12-02 21:31:57 -0800

    acceptance criteria met

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,25 @@
-// const btnEl = document.querySelectorAll('button');
-
-// var advancedFormat = require('dayjs/plugin/advancedFormat');
-// dayjs.extend(advancedFormat);
 let today = dayjs().format('dddd, MMMM D');
+let currentHour = dayjs().hour();
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-// to satisfy the above requirement, do I use window.on("load") or document.ready?
 $(document).ready(function() {
-  // set current Date above planner table
+  // set current Date at bottom of header
   $('#currentDay').text(today);
   
-  // get localstorage info and display in appropriate textarea
+  // forEach loop for textarea elements - sets local storage date & bkgrnd color based on hour
   $('textarea').each(function() {
+    // Check localStorage for saved data and set in appropriate textarea box
     let hourId = $(this).parent().attr('id');
     let activityText = localStorage.getItem(hourId + 'activity');
     $(this).val(activityText);
-  })
+    // compare the time-block of the textarea to the current hour of the day and apply appropriate bkgrnd color
+    if (hourId == currentHour) {
+      $(this).parent().addClass("present");
+    } else if (hourId < currentHour) {
+      $(this).parent().addClass("past");
+    } else {
+      $(this).parent().addClass("future");
+    }
+  });
 });
 
 // function that adds button eventlistener and saves textarea (aka activities) to local storage
@@ -27,22 +28,6 @@ $(function () {
     let hourId = $(this).parent().attr('id');
     let activity = $('#' + hourId).children('textarea').val();
     localStorage.setItem(hourId + 'activity', activity);
+    $('#savedMsg').text('Appointment Saved to Local Storage');
   });
-
-
-
-
- 
-  
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
